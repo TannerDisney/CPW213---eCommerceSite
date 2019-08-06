a108d04eb76a89bcb1400972f3fed72e21bae047
ZelphionGaming <disneytanner@gmail.com>
2019-08-06 16:58:18 -0700

    Created LibraryController and ability to add games to the database

---
+++ eCommerce/Controllers/LibraryController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eCommerce.Data;
+using eCommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.Controllers
+{
+    public class LibraryController : Controller
+    {
+        private readonly GameContext _context;
+
+        public LibraryController(GameContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(VideoGame game)
+        {
+            if (ModelState.IsValid)
+            {
+                // Add to Database
+                VideoGameDb.Add(game, _context);
+                return RedirectToAction("Index");
+            }
+
+            // Return view with model including error messages
+            return View(game);
+        }
+    }
+}
---
+++ eCommerce/Data/VideoGameDb.cs
@@ -0,0 +1,21 @@
+﻿using eCommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerce.Data
+{
+    /// <summary>
+    /// Db helper class for video games
+    /// </summary>
+    public static class VideoGameDb
+    {
+        public static VideoGame Add(VideoGame g, GameContext context)
+        {
+            context.Add(g);
+            context.SaveChanges();
+            return g;
+        }
+    }
+}
