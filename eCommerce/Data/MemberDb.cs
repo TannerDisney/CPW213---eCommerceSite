b095e3da30dc0d863562567f287c26596123b309
Tanner Disney <tanner_disney@yahoo.com>
2019-08-13 09:28:39 -0700

    Created function registration to the website

---
+++ eCommerce/Data/MemberDb.cs
@@ -0,0 +1,25 @@
+﻿using eCommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerce.Data
+{
+    public static class MemberDb
+    {
+        /// <summary>
+        /// Adds a member to the database. returns the member
+        /// with their MemberId populated
+        /// </summary>
+        /// <param name="context">The database context</param>
+        /// <param name="m">The new member to be added</param>
+        /// <returns></returns>
+        public async static Task<Member> Add(GameContext context, Member m)
+        {
+            context.Members.Add(m);
+            await context.SaveChangesAsync();
+            return m;
+        }
+    }
+}
