8948b254f2971fc12bb659e6a6c38e7a3e23a77e
Tanner Disney <tanner_disney@yahoo.com>
2019-08-20 10:40:47 -0700

    Started Cartcontroller need to finish later

---
+++ eCommerce/Controllers/CartController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eCommerce.Data;
+using eCommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly GameContext _context;
+
+        public CartController(GameContext context)
+        {
+            _context = context;
+        }
+        // Add the game to a cookie
+        public async Task<IActionResult> Add(int id)
+        {
+            VideoGame g = await VideoGameDb.GetGameById(id, _context);
+            return View();
+        }
+    }
+}
