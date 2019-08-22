b271414afc8d8b462b0a8040b275cd40e946a399
Tanner Disney <tanner_disney@yahoo.com>
2019-08-22 09:40:59 -0700

    Added CartHelper class to project to aid in keeping track of cartitems

--- eCommerce/Controllers/CartController.cs
+++ eCommerce/Controllers/CartController.cs
@@ -4,23 +4,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using eCommerce.Data;
 using eCommerce.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace eCommerce.Controllers
 {
     public class CartController : Controller
     {
+        private readonly IHttpContextAccessor _httpAccessor;
         private readonly GameContext _context;
 
-        public CartController(GameContext context)
+        public CartController(GameContext context, IHttpContextAccessor httpAccessor)
         {
             _context = context;
+            _httpAccessor = httpAccessor;
         }
         // Add the game to a cookie
         public async Task<IActionResult> Add(int id)
         {
             VideoGame g = await VideoGameDb.GetGameById(id, _context);
-            return View();
+            string data = JsonConvert.SerializeObject(g);
+            CookieOptions options = new CookieOptions()
+            {
+                Secure = true,
+                MaxAge = TimeSpan.FromDays(14)
+            };
+
+            _httpAccessor.HttpContext.Response.Cookies.Append("CartCookie", data, options);
+            return RedirectToAction("Index", "Library");
         }
     }
 }
